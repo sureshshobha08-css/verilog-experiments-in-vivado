a663c9ddab2bebccfa5cdbedfdd9988505d6319a
Shobha J S <sureshshobha54@gmail.com>
2026-07-31 23:47:17 +0530

    Update testbench.v

--- boolean expression/testbench.v
+++ boolean expression/testbench.v
@@ -1,1 +1,17 @@
-
+module top;
+reg a,b,c,d;
+wire y;
+boolean_expression u1(a,b,c,d,y);
+initial
+begin
+a=0;b=1;c=1;d=0;
+#10;a=1;b=1;c=0;d=0;
+#10;a=1;b=1;c=1;d=0;
+#10;a=1;b=1;c=1;d=1;
+end
+initial
+begin
+#40 $finish;
+$monitor("time=%0t a=%b b=%b c=%b d=%b y=%b",$time,a,b,c,d,y);
+end
+endmodule
