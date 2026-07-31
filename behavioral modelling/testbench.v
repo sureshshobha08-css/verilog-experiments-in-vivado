e4f800d82013d0f78024eaf6abc94c753e7a03f4
Shobha J S <sureshshobha54@gmail.com>
2026-07-31 08:04:58 +0530

    Update testbench.v

--- behavioral modelling/testbench.v
+++ behavioral modelling/testbench.v
@@ -1,1 +1,23 @@
+module top;
+reg a,b;
+reg [2:0]opcode;
+wire y;
+behavioral_level uut(a,b,opcode,y);
+initial 
+begin
+a=0;b=0;
+opcode=3'b000;
+#10;opcode=3'b001;
+#10;opcode=3'b010;
+#10;opcode=3'b011;
+#10;opcode=3'b100;
+#10;opcode=3'b101;
+#10;opcode=3'b110;
+#10;opcode=3'b111;
+#10;
 
+#10 $finish;
+end
+initial
+$monitor("time=%0t a=%b b=%b opcode=%b y=%b",$time,a,b,opcode,y);
+endmodule
