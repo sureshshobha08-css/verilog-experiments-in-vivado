1a0be8f9ccb3cddc4f6c64891ba6564b79ae34da
Shobha J S <sureshshobha54@gmail.com>
2026-07-31 08:03:46 +0530

    Update source.v

--- behavioral modelling/source.v
+++ behavioral modelling/source.v
@@ -1,1 +1,20 @@
+module behavioral_level(a,b,opcode,y);
+input a,b;
+input[2:0]opcode;
+output reg y;
+always@(*)
+begin
+case(opcode)
+3'b000:y=a&b;
+3'b001:y=a|b;
+3'b010:y=~a;
+3'b011:y=a;
+3'b100:y=~(a&b);
+3'b101:y=~(a|b);
+3'b110:y=a^b;
+3'b111:y=~(a^b);
+default:y=1'bX;
+endcase
+end
+endmodule
 
