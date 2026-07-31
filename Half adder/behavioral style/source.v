11b439e94ac321a28d2205f5762d6c53dc2885d1
Shobha J S <sureshshobha54@gmail.com>
2026-08-01 00:12:09 +0530

    Update source.v

--- Half adder/behavioral style/source.v
+++ Half adder/behavioral style/source.v
@@ -1,1 +1,12 @@
-
+module half_adder(input a,b,output reg sum,cout);
+always@(*)
+begin
+case({a,b})
+2'b00:{sum,cout}=2'b00;
+2'b01:{sum,cout}=2'b10;
+2'b10:{sum,cout}=2'b10;
+2'b11:{sum,cout}=2'b01;
+default:{sum,cout}=1'bx;
+endcase
+end
+endmodule
