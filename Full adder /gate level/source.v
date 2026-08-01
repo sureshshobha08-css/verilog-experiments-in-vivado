7ac03b6f239445b0b1511409a57b943bcb076b58
Shobha J S <sureshshobha54@gmail.com>
2026-08-01 22:10:43 +0530

    Update source.v

--- Full adder /gate level/source.v
+++ Full adder /gate level/source.v
@@ -1,4 +1,8 @@
 module full_adder(input a,b,cin,output sum,cout);
-assign sum=a^b^cin;
-assign cout=((a&b)|(b&cin)|(cin&a));
+wire w1,w2,w3;
+xor x1(sum,a,b,cin);
+and a1(w1,a,b);
+and a2(w2,b,cin);
+and a3(w3,cin,a);
+or o1(cout,w1,w2,w3);
 endmodule
