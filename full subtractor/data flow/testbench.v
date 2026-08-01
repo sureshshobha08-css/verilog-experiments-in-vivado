fa5034190c2e05c824d755c846a7e864b4645281
Shobha J S <sureshshobha54@gmail.com>
2026-08-02 00:29:09 +0530

    Update testbench.v

--- full subtractor/data flow/testbench.v
+++ full subtractor/data flow/testbench.v
@@ -1,1 +1,17 @@
+module top;
+reg a,b,cin;
+wire D,B;
+full_subtractor u1(a,b,cin,D,B);
+initial
+begin
+a=0;b=0;cin=0;
+#10;a=0;b=0;cin=1;
+#10;a=0;b=1;cin=0;
+#10;a=0;b=1;cin=1;
+#10;a=1;b=0;cin=0;
+#10;a=1;b=0;cin=1;
+#10;a=1;b=1;cin=0;
+#10;a=1;b=1;cin=1;
+#10 $finish;
+end
 
