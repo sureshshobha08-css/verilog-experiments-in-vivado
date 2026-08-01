ec218951dddf17e3edd81b8d6407f7d5142b36e6
Shobha J S <sureshshobha54@gmail.com>
2026-08-01 22:17:41 +0530

    Update source.v

--- Full adder /behavioral style/source.v
+++ Full adder /behavioral style/source.v
@@ -1,1 +1,18 @@
 
+
+module full_adder(input a,b,cin,output reg sum,cout);
+always@(*)
+begin
+case({a,b,cin})
+3'b000:{sum,cout}=2'b00;
+3'b001:{sum,cout}=2'b10;
+3'b010:{sum,cout}=2'b10;
+3'b011:{sum,cout}=2'b01;
+3'b100:{sum,cout}=2'b10;
+3'b101:{sum,cout}=2'b01;
+3'b110:{sum,cout}=2'b01;
+3'b111:{sum,cout}=2'b11;
+default:{sum,cout}=1'bx;
+endcase
+end
+endmodule
