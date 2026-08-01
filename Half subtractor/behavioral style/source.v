14b996ac09524e4457d783e06b956c9979310530
Shobha J S <sureshshobha54@gmail.com>
2026-08-02 00:40:03 +0530

    Update source.v

--- Half subtractor/behavioral style/source.v
+++ Half subtractor/behavioral style/source.v
@@ -1,1 +1,12 @@
-
+module half_subtractor(input a,b,output reg D,B);
+always@(*)
+begin
+case({a,b})
+2'b00:{D,B}=2'b00;
+2'b01:{D,B}=2'b11;
+2'b10:{D,B}=2'b10;
+2'b11:{D,B}=2'b00;
+default:{D,B}=1'bx;
+endcase
+end
+endmodule
