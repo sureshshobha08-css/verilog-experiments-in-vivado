2c5194bc4b6884558e739552d3cec50e0cd7db65
Shobha J S <sureshshobha54@gmail.com>
2026-08-01 22:42:42 +0530

    Update testbench.v

--- Full adder using 2 half adder/testbench.v
+++ Full adder using 2 half adder/testbench.v
@@ -1,1 +1,17 @@
-
+module top;
+reg a,b,cin;
+wire sum,cout;
+full_adder u1(a,b,cin,sum,cout);
+initial
+begin
+a=0;b=0;cin=0;
+#10;a=0;b=0;cin=1;
+#10;a=0;b=1;cin=0;
+#10;a=0;b=1;cin=1;
+#10;a=1;b=0;cin=0;
+#10;a=1;b=0;cin=1;
+#10;a=1;b=1;cin=1;
+#10;
+#10 $finish;
+end
+endmodule
