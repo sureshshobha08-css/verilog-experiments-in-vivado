0dd6f0ad72d9dd38dc5669b35dd24b6c9fa90e93
Shobha J S <sureshshobha54@gmail.com>
2026-08-02 00:22:56 +0530

    Update source.v

--- full subtractor/gate level/source.v
+++ full subtractor/gate level/source.v
@@ -1,1 +1,9 @@
-
+module full_subtractor(input a,b,cin,output D,B);
+wire w1,w2,w3,w4;
+xor x1(D,a,b,cin);
+xor x2(w2,b,cin);
+not n1(w1,a);
+and a1(w3,w1,w2);
+and a2(w4,b,cin);
+or o1(B,w3,w4);
+endmodule
