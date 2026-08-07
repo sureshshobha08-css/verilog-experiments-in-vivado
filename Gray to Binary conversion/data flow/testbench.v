a146d31c44396660a999d921fa5f87e6e24fc27c
Shobha J S <sureshshobha54@gmail.com>
2026-08-07 20:28:28 +0530

    Update testbench.v

--- Gray to Binary conversion/data flow/testbench.v
+++ Gray to Binary conversion/data flow/testbench.v
@@ -1,1 +1,18 @@
+module top;
+reg [2:0]g;
+wire [2:0]b;
+GRAYtoB u1(g,b);
+initial
+begin
+g=3'b000;
+#10;g=3'b001;
+#10;g=3'b010;
+#10;g=3'b011;
+#10;g=3'b100;
+#10;g=3'b101;
+#10;g=3'b110;
+#10;g=3'b111;
+#10 $finish;
+end
+endmodule
 
