2928651d57e840e666c0eb7d72612c2782f28d05
Shobha J S <sureshshobha54@gmail.com>
2026-08-07 19:58:44 +0530

    Update testbench.v

--- Binary to gray conversion/gate level/testbench.v
+++ Binary to gray conversion/gate level/testbench.v
@@ -1,1 +1,17 @@
-
+module top;
+reg [2:0]b;
+wire [2:0]g;
+BtoGRAY u1(b,g);
+initial
+begin
+b=3'b000;
+#10;b=3'b001;
+#10;b=3'b010;
+#10;b=3'b011;
+#10;b=3'b100;
+#10;b=3'b101;
+#10;b=3'b110;
+#10;b=3'b111;
+#10 $finish;
+end
+endmodule
