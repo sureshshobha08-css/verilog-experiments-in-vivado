e7ead03d41d73a30bdfea7aa64ddffc02cf79f05
Shobha J S <sureshshobha54@gmail.com>
2026-08-07 22:08:58 +0530

    Update testbench.v

--- Excess-3 to BCD/behavioral style/testbench.v
+++ Excess-3 to BCD/behavioral style/testbench.v
@@ -1,1 +1,21 @@
+module top;
+reg [3:0]e;
+wire [3:0]b;
+Excess3toBCD u1(e,b);
+initial
+begin
+e=4'b0011;
+#10;e=4'b0100;
+#10;e=4'b0101;
+#10;e=4'b0110;
+#10;e=4'b0111;
+#10;e=4'b1000;
+#10;e=4'b1001;
+#10;e=4'b1010;
+#10;e=4'b1011;
+#10;e=4'b1100;
+
+#10 $finish;
+end
+endmodule
 
