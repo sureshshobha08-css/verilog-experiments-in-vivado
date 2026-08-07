0957502bf29c3c8275d44e6065ba3a119701bd22
Shobha J S <sureshshobha54@gmail.com>
2026-08-07 21:32:17 +0530

    Update testbench.v

--- BCD to Excess-3/gate level/testbench.v
+++ BCD to Excess-3/gate level/testbench.v
@@ -1,1 +1,20 @@
+module top;
+reg [3:0]b;
+wire [3:0]e;
+BCDtoExcess3 u1(b,e);
+initial
+begin
+b=4'b0000;
+#10;b=4'b0001;
+#10;b=4'b0010;
+#10;b=4'b0011;
+#10;b=4'b0100;
+#10;b=4'b0101;
+#10;b=4'b0110;
+#10;b=4'b0111;
+#10;b=4'b1000;
+#10;b=4'b1001;
 
+#10 $finish;
+end
+endmodule
