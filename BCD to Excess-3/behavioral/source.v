4104521eb9de75086c2a3fae58be26b3a7e3bfd6
Shobha J S <sureshshobha54@gmail.com>
2026-08-07 21:33:23 +0530

    Update source.v

--- BCD to Excess-3/behavioral/source.v
+++ BCD to Excess-3/behavioral/source.v
@@ -1,1 +1,20 @@
 
+
+module BCDtoExcess3(input[3:0]b,output reg [3:0]e);
+always@(*)
+begin
+case(b)
+4'b0000:e=4'b0011;
+4'b0001:e=4'b0100;
+4'b0010:e=4'b0101;
+4'b0011:e=4'b0110;
+4'b0100:e=4'b0111;
+4'b0101:e=4'b1000;
+4'b0110:e=4'b1001;
+4'b0111:e=4'b1010;
+4'b1000:e=4'b1011;
+4'b1001:e=4'b1100;
+default:e=4'bxxxx;
+endcase 
+end 
+endmodule
