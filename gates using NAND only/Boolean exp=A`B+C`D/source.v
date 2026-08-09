90e9b258d6e1cdb29748579a6395dfb8782b28b1
Shobha J S <sureshshobha54@gmail.com>
2026-08-09 18:52:29 +0530

    Update source.v

--- gates using NAND only/Boolean exp=A`B+C`D/source.v
+++ gates using NAND only/Boolean exp=A`B+C`D/source.v
@@ -1,1 +1,9 @@
 
+module boolean_exp_using_nandgate(input a,b,c,d,output y);
+wire w1,w2,w3,w4;
+nand n1(w1,a,a); 
+nand n2(w3,w1,b);
+nand n3(w2,c,c);
+nand n4(w4,w2,d);
+nand n5(y,w3,w4);
+endmodule
