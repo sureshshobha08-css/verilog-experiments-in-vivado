1e8cfb62498953d670344fb0a4f7e230cad5b5ff
Shobha J S <sureshshobha54@gmail.com>
2026-08-09 18:57:46 +0530

    Update tb.v

--- gates using  NOR only/Boolean exp=(A`+B).(C`+D)/tb.v
+++ gates using  NOR only/Boolean exp=(A`+B).(C`+D)/tb.v
@@ -1,1 +1,14 @@
 
+module top;
+reg a,b,c,d;
+wire y;
+boolean_exp_using_norgate u1(a,b,c,d,y);
+initial
+begin
+a=0;b=0;c=0;d=0;
+#10; a=0;b=1;c=0;d=1;
+#10; a=1;b=0;c=1;d=0;
+#10; a=0;b=0;c=0;d=1;
+#10 $finish;
+end
+endmodule
