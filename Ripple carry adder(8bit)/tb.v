60dc4af0b64762fc81a0b2064587f7d32851c142
Shobha J S <sureshshobha54@gmail.com>
2026-08-12 21:00:27 +0530

    Update tb.v

--- Ripple carry adder(8bit)/tb.v
+++ Ripple carry adder(8bit)/tb.v
@@ -1,1 +1,13 @@
-
+module top;
+reg [7:0]a,b;
+reg cin;
+wire [7:0]sum;
+wire cout;
+ripple_carry_adder_8bit u1(a,b,cin,sum,cout);
+initial 
+begin
+a=8'b11110000;b=8'b00001111;cin=0;
+#10; a=8'b11101100; b=8'b10100001;cin=1;
+#10 $finish;
+end
+endmodule
