2795bc0e877555b7756a42dbec91caa9962ca18a
Shobha J S <sureshshobha54@gmail.com>
2026-08-12 20:41:18 +0530

    Update tb.v

--- Ripple carry adder(4bit)/tb.v
+++ Ripple carry adder(4bit)/tb.v
@@ -1,1 +1,27 @@
-
+module top;
+reg [3:0]a,b;
+reg cin;
+wire [3:0]sum;
+wire cout;
+ripple_carry_adder_4bit u1(a,b,cin,sum,cout);
+initial 
+begin
+a=4'b11111;b=4'b0000;cin=0;
+#10; a=4'b1110; b=4'b0001;cin=1;
+#10; a=4'b1101; b=4'b0010;cin=0;
+#10; a=4'b1100; b=4'b0011;cin=0;
+#10; a=4'b1011; b=4'b0100;cin=1;
+#10; a=4'b1010; b=4'b0101;cin=0;
+#10; a=4'b1001; b=4'b0110;cin=1;
+#10; a=4'b1000; b=4'b0111;cin=0;
+#10; a=4'b0111; b=4'b1000;cin=1;
+#10; a=4'b0110; b=4'b1001;cin=1;
+#10; a=4'b0101; b=4'b1010;cin=1;
+#10; a=4'b0100; b=4'b1011;cin=0;
+#10; a=4'b0011; b=4'b1100;cin=0;
+#10; a=4'b0010; b=4'b1101;cin=0;
+#10; a=4'b0001; b=4'b1110;cin=1;
+#10;a=4'b0000 ;b=4'b1111;cin=0;
+#10 $finish;
+end
+endmodule
