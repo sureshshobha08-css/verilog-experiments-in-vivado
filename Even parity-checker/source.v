dce44825ee0212b729dda1659598dc6c2086cbc5
Shobha J S <sureshshobha54@gmail.com>
2026-08-12 21:59:34 +0530

    Update source.v

--- Even parity-checker/source.v
+++ Even parity-checker/source.v
@@ -1,1 +1,14 @@
 
+module even_parity_checker(input [3:0]data,
+                           input parity,m,
+                           output reg checker,output reg error);
+always@(*)
+begin
+if(m)
+begin
+checker=^{data,parity};
+error=checker;
+end
+end
+endmodule
+
