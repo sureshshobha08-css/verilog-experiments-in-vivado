d24f97d85343b24b374e2a418ea07692c686ecad
Shobha J S <sureshshobha54@gmail.com>
2026-08-12 22:13:25 +0530

    Update source.v

--- Odd parity-checker/source.v
+++ Odd parity-checker/source.v
@@ -1,1 +1,13 @@
 
+module odd_parity_checker(input [3:0]data,
+                           input parity,m,
+                           output reg checker,output reg error);
+always@(*)
+begin
+if(m)
+begin
+checker=~^{data,parity};
+error=checker;
+end
+end
+endmodule
