267c53d0ad8b9692844b8832dfc7d55c25891b6a
Shobha J S <sureshshobha54@gmail.com>
2026-08-12 19:28:26 +0530

    Update source.v

--- seven segment display/source.v
+++ seven segment display/source.v
@@ -1,1 +1,19 @@
+module seven_segment_display(input[3:0]b,output reg[6:0] y );
+always@(*)
+begin
+case(b)
+4'b0000:y=7'b1111110;
+4'b0001:y=7'b0110000;
+4'b0010:y=7'b1101101;
+4'b0011:y=7'b1111001;
+4'b0100:y=7'b0110011;
+4'b0101:y=7'b1011011;
+4'b0110:y=7'b1011111;
+4'b0111:y=7'b1110000;
+4'b1000:y=7'b1111110;
+4'b1001:y=7'b1110011;
+default:y=7'bxxxxxxx;
+endcase 
+end
+endmodule
 
