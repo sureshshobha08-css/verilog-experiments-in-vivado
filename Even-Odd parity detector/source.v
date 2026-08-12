e26208f1737ebdd50a53756aac3c712a3b88967f
Shobha J S <sureshshobha54@gmail.com>
2026-08-12 22:29:37 +0530

    Update source.v

--- Even-Odd parity detector/source.v
+++ Even-Odd parity detector/source.v
@@ -1,1 +1,14 @@
 
+module odd_parity_checker(input [3:0]data,
+                           input parity,m,
+                           output reg checker,reg even_parity,odd_parity);
+always@(*)
+begin
+if(m)
+begin
+checker=^{data,parity};
+even_parity=checker; //if checker=0--->even
+odd_parity=~checker; //if checker=0--->odd
+end
+end
+endmodule
