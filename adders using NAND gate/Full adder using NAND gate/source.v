c35ea929bd0beab76238c8ecdc46045a8df38fcb
Shobha J S <sureshshobha54@gmail.com>
2026-08-18 15:58:19 +0530

    Update source.v

--- adders using NAND gate/Full adder using NAND gate/source.v
+++ adders using NAND gate/Full adder using NAND gate/source.v
@@ -1,1 +1,13 @@
+module full_adder_by_nand(input a,b,cin,output sum,cout);
+wire w1,w2,w3,w4,w5,w6,w7;
+nand n1(w1,a,b);
+nand n2(w2,a,w1);
+nand n3(w3,w1,b);
+nand n4(w4,w2,w3);
+nand n5(w5,w4,cin);
+nand n6(w6,w4,w5);
+nand n7(w7,w5,cin);
+nand n8(sum,w6,w7);
+nand n9(cout,w5,w1);
+endmodule
 
