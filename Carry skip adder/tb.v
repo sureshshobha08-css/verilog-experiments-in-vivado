38fe8cda48a25392b0fb7a716dabb344e22aacd3
Shobha J S <sureshshobha54@gmail.com>
2026-08-24 15:12:40 +0530

    Update tb.v

--- Carry skip adder/tb.v
+++ Carry skip adder/tb.v
@@ -1,15 +1,16 @@
 module top;
-reg[3:0]A,B,C;
-wire[4:0]Sum;
+reg[3:0]A,B;
+reg Cin;
+wire[3:0]Sum;
 wire cout;
-carry_save_adder u1(A,B,C,Sum,cout);
+carry_bypass_skip_adder u1(A,B,Cin,Sum,cout);
 initial
 begin
-A=4'b1000;B=4'b0010;C=4'b0111;
-#10;A=4'b0101;B=4'b0111;C=4'b1111;
-#10;A=4'b0110;B=4'b0100;C=4'b1100;
-#10;A=4'b1101;B=4'b0010;C=4'b1011;
-#10;A=4'b1111;B=4'b1111;C=4'b1111;
+A=4'b1000;B=4'b0010;Cin=4'b0;
+#10;A=4'b0101;B=4'b0111;Cin=4'b1;
+#10;A=4'b0110;B=4'b0100;Cin=4'b0;
+#10;A=4'b1101;B=4'b0010;Cin=4'b1;
+#10;A=4'b1111;B=4'b1111;Cin=4'b0;
 #10 $finish;
 end
 endmodule
