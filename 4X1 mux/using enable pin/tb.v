74da9fa96a664dd5ad4b89431b76256468708a80
Shobha J S <sureshshobha54@gmail.com>
2026-08-25 14:48:51 +0530

    Update tb.v

--- 4X1 mux/using enable pin/tb.v
+++ 4X1 mux/using enable pin/tb.v
@@ -1,1 +1,17 @@
 
+module top;
+reg i0,i1,i2,i3,s0,s1,E;
+wire y;
+mux u1(i0,i1,i2,i3,s0,s1,E,y);
+initial
+begin 
+i0=1'b0;i1=1'b1;i2=1'b0;i3=1'b1;
+E=0;s1=0;s0=0;
+#10;s1=0;s0=1;
+#10;E=1;s1=1;s0=0;
+#10;s1=0;s0=1;
+#10;s1=1;s0=1;
+#10 $finish;
+end
+endmodule
+
