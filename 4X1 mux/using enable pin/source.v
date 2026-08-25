4cb92a88f20088f14bc45a36996d4876028b55c7
Shobha J S <sureshshobha54@gmail.com>
2026-08-25 14:48:22 +0530

    Update source.v

--- 4X1 mux/using enable pin/source.v
+++ 4X1 mux/using enable pin/source.v
@@ -1,1 +1,19 @@
 
+module mux(input i0,i1,i2,i3,s0,s1,E,output reg y);
+always@(*)
+begin 
+if(E==1)
+begin
+case({s1,s0})
+2'b00:y=i0;
+2'b01:y=i1;
+2'b10:y=i2;
+2'b11:y=i3;
+default:y=1'bx;
+endcase
+end
+else 
+y=1'b00;
+end
+endmodule
+
