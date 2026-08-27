1769ed9d8066aa80fd2f5984ca0b09b6207cbe02
Shobha J S <sureshshobha54@gmail.com>
2026-08-27 22:25:20 +0530

    Update source.v

--- 8X1 mux/using if-ifelse statements/source.v
+++ 8X1 mux/using if-ifelse statements/source.v
@@ -1,1 +1,22 @@
 
+module mux(input[7:0]i,input[2:0]s,output reg y;
+always@(*)
+begin
+if(s[2:0]==3'b000)
+y=i[0];
+else if(s[2:0]==3'b001)
+y=i[1];
+else if(s[2:0]==3'b010)
+y=i[2];
+else if(s[2:0]==3'b011)
+y=i[3];
+else if(s[2:0]==3'b100)
+y=i[4];
+else if(s[2:0]==3'b101)
+y=i[5];
+else if(s[2:0]==3'b110)
+y=i[6];
+else 
+y=i[7]; 
+end
+endmodule
