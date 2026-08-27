8b64be9af7231d3cf693086d8fd44b31d0472a4a
Shobha J S <sureshshobha54@gmail.com>
2026-08-27 22:12:59 +0530

    Update tb.v

--- 8X1 mux/gate level modelling/tb.v
+++ 8X1 mux/gate level modelling/tb.v
@@ -1,1 +1,21 @@
 
+module top;
+reg[7:0]i;
+reg[2:0]s;
+wire y;
+mux u1(i,s,y);
+initial
+begin 
+i=8'b10101010;
+s=3'b000;
+#10;s=3'b001;
+#10;s=3'b010;
+#10;s=3'b011;
+#10;s=3'b100;
+#10;s=3'b101;
+#10;s=3'b110;
+#10;s=3'b111;
+#10 $finish;
+end
+endmodule
+
