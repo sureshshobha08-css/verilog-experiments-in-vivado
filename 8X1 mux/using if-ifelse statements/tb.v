65462993f9d6d536fb8d0629a13e9507a658f57a
Shobha J S <sureshshobha54@gmail.com>
2026-08-27 22:21:15 +0530

    Update tb.v

--- 8X1 mux/using if-ifelse statements/tb.v
+++ 8X1 mux/using if-ifelse statements/tb.v
@@ -1,1 +1,19 @@
-
+module top;
+reg[7:0]i;
+reg[2:0]s;
+wire y;
+mux u1(i,s,y);
+initial
+begin 
+i=8'b00101011;
+s=3'b000;
+#10;s=3'b001;
+#10;s=3'b010;
+#10;s=3'b011;
+#10;s=3'b100;
+#10;s=3'b101;
+#10;s=3'b110;
+#10;s=3'b111;
+#10 $finish;
+end
+endmodule
