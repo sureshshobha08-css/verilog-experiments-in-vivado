8c86eec24147878b1aa09d3bb9d17a3c3adb2e0c
Shobha J S <sureshshobha54@gmail.com>
2026-08-28 14:47:41 +0530

    Update source.v

--- 1X2 demux/using if-else if statements/source.v
+++ 1X2 demux/using if-else if statements/source.v
@@ -1,14 +1,15 @@
 module demux(input i,s,output reg y0,y1);
 always@(*)
 begin
-case(s)
-1'b0:begin
-y0=i;y1=0;
-end
-1'b1:begin
+if(s==0)
+    begin
+y0=i;
+y1=0;
+    end
+else
+    begin
 y1=i;
 y0=0;
-end
-endcase
+    end
 end
 endmodule
