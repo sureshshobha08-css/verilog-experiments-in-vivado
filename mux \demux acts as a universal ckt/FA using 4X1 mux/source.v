c33c829bd0f2a1d07f248ec0c3360087a2b5c01f
Shobha J S <sureshshobha54@gmail.com>
2026-08-28 22:29:02 +0530

    Update source.v

--- mux \demux acts as a universal ckt/FA using 4X1 mux/source.v
+++ mux \demux acts as a universal ckt/FA using 4X1 mux/source.v
@@ -1,1 +1,4 @@
-
+odule FA_4X1_mux(input a,b,cin,output sum,cout);
+assign sum=({a,b}==2'b00)?cin:({a,b}==2'b01)?~cin:({a,b}==2'b10)?~cin:cin;
+assign cout=({a,b}==2'b00)?0:({a,b}==2'b01)?cin:({a,b}==2'b10)?cin:1;
+endmodule
