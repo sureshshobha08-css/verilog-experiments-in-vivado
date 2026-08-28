ba77ff244d9a77ff3a0c60ef29054b06a68de601
Shobha J S <sureshshobha54@gmail.com>
2026-08-28 15:40:18 +0530

    Create source.v

---
+++ 1X4 demux/behavioral style/source.v
@@ -0,0 +1,23 @@
+module demux(input i,s1,s0,output reg y0,y1,y2,y3);
+always@(*)
+begin
+case({s1,s0})
+2'b00:begin
+y0=i;
+y1=0;y2=0;y3=0;
+end
+2'b01:begin
+y0=0;
+y1=i;y2=0;y3=0;
+end
+2'b10:begin
+y0=0;
+y1=0;y2=i;y3=0;
+end
+2'b11:begin
+y0=0;
+y1=0;y2=0;y3=i;
+end
+endcase
+end
+endmodule
