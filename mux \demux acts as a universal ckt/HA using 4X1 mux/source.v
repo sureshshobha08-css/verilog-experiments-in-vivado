39996d95c4a58bdbbbf91e4d79ad826907e4c594
Shobha J S <sureshshobha54@gmail.com>
2026-08-28 22:21:25 +0530

    Update source.v

--- mux \demux acts as a universal ckt/HA using 4X1 mux/source.v
+++ mux \demux acts as a universal ckt/HA using 4X1 mux/source.v
@@ -1,1 +1,4 @@
-
+module HA_4X1_mux(input a,b,output sum,cout);
+assign sum=({a,b}==2'b00)?0:({a,b}==2'b01)?1:({a,b}==2'b10)?1:0;
+assign cout=({a,b}==2'b00)?0:({a,b}==2'b01)?0:({a,b}==2'b10)?0:1;
+endmodule
