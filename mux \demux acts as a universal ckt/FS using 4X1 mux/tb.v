e9992e1df49bf92c094b27a423632d25f9179907
Shobha J S <sureshshobha54@gmail.com>
2026-08-28 23:10:15 +0530

    Create tb.v

---
+++ mux \demux acts as a universal ckt/FS using 4X1 mux/tb.v
@@ -0,0 +1,17 @@
+module top;
+reg a,b,cin;
+wire D,BO;
+FS_4X1_mux u1( a,b,cin,D,BO); 
+initial 
+begin
+a=0;b=0;cin=0;
+#10;a=0;b=0;cin=1;
+#10;a=0;b=1;cin=0;
+#10;a=0;b=1;cin=1;
+#10;a=1;b=0;cin=0;
+#10;a=1;b=0;cin=1;
+#10;a=1;b=1;cin=0;
+#10;a=1;b=1;cin=1;
+#10 $finish;
+end
+endmodule
