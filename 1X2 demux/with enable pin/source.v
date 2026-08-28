ec12fd48a814cb7c8ab125936c3dd11c5ea8bc77
Shobha J S <sureshshobha54@gmail.com>
2026-08-28 15:09:54 +0530

    Update source.v

--- 1X2 demux/with enable pin/source.v
+++ 1X2 demux/with enable pin/source.v
@@ -1,14 +1,19 @@
-module demux(input i,s,output reg y0,y1);
+module demux(input i,e,s,output reg y0,y1);
 always@(*)
 begin
-case(s)
-1'b0:begin
-y0=i;y1=0;
+if(e==1'b1)
+
+begin
+y0=(s==0)?i:1'b0;
+y1=(s==1)?i:1'b0;
 end
-1'b1:begin
-y1=i;
-y0=0;
+
+else
+
+begin
+y0=1'b0;
+y1=1'b0;
 end
-endcase
+
 end
 endmodule
