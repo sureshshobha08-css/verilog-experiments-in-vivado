d9b313c221eb421dde7909e9b18d490ef613dd64
Shobha J S <sureshshobha54@gmail.com>
2026-08-29 19:31:10 +0530

    Update tb.v

--- Octal to binary encoder/tb.v
+++ Octal to binary encoder/tb.v
@@ -1,1 +1,18 @@
 
+module top;
+reg [7:0]octal_in;
+wire[2:0]binary_out;
+octal_to_binary_encoder u1(octal_in,binary_out);
+initial 
+begin 
+octal_in=8'b00000001;
+#10;octal_in=8'b00000010;
+#10;octal_in=8'b00000100;
+#10;octal_in=8'b00001000;
+#10;octal_in=8'b00010000;
+#10;octal_in=8'b00100000;
+#10;octal_in=8'b01000000;
+#10;octal_in=8'b10000000;
+#10 $finish;
+end
+endmodule
