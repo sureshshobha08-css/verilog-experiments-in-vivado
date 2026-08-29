a77fc7a44da48e1ee4f292f653c5f44a5248c9e1
Shobha J S <sureshshobha54@gmail.com>
2026-08-29 18:44:54 +0530

    Create tb.v

---
+++ full adder using 3X8 decoder/full adder using 3X8 decoder/using NOR gate at output side/tb.v
@@ -0,0 +1,17 @@
+module top;
+reg a,b,cin;
+wire sum,cout;
+FA_decoder_3x8_NOR u1 (a,b,cin,sum,cout);
+initial 
+begin 
+a=0;b=0;cin=0;
+#10;a=0;b=0;cin=1;
+#10;a=0;b=1;cin=0;
+#10;a=0;b=1;cin=1;
+#10;a=1;b=0;cin=0;
+#10;a=1;b=0;cin=1;
+#10;a=1;b=1;cin=0;
+#10;a=1;b=1;cin=1;
+#10 $finish;
+end
+endmodule
