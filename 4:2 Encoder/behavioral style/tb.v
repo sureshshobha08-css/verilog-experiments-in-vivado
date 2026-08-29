bb958076fc6809939e01b8ec061f7201fc80d3e4
Shobha J S <sureshshobha54@gmail.com>
2026-08-29 15:51:09 +0530

    Create tb.v

---
+++ 4:2 Encoder/behavioral style/tb.v
@@ -0,0 +1,14 @@
+module top;
+reg i0,i1,i2,i3;
+wire y0,y1;
+encoder_4x2 u1(i0,i1,i2,i3,y0,y1);
+initial 
+begin 
+i3=0;i2=0;i1=0;i0=0;
+#10;i3=0;i2=0;i1=0;i0=1;
+#10;i3=0;i2=0;i1=1;i0=0;
+#10;i3=0;i2=1;i1=0;i0=0;
+#10;i3=1;i2=0;i1=0;i0=0;
+#10 $finish;
+end
+endmodule
