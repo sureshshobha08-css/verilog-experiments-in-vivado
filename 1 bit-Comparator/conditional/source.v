86a788e5badfc3bec8f336a3200a9a7d6fc1eec5
Shobha J S <sureshshobha54@gmail.com>
2026-08-29 22:18:21 +0530

    Update source.v

--- 1 bit-Comparator/conditional/source.v
+++ 1 bit-Comparator/conditional/source.v
@@ -1,1 +1,12 @@
-
+module comparator_1bit(input a,b,output reg equal,greater,smaller);
+always@(*)
+begin
+equal=0;greater=0;smaller=0;
+if(a==b)
+equal=~(a^b);
+else if(a>b)
+greater=a&(~b);
+else 
+smaller=~a&b;
+end
+endmodule
