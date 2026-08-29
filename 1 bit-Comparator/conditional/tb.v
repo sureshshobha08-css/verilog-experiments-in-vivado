11628293fd8457695f6f4b81c5b6159212c73881
Shobha J S <sureshshobha54@gmail.com>
2026-08-29 22:17:52 +0530

    Update tb.v

--- 1 bit-Comparator/conditional/tb.v
+++ 1 bit-Comparator/conditional/tb.v
@@ -1,1 +1,13 @@
-
+module top;
+reg a,b;
+wire equal,greater,smaller;
+comparator_1bit u1(a,b,equal,greater,smaller);
+initial
+begin
+a=0;b=0;
+#10;a=0;b=1;
+#10;a=1;b=0;
+#10;a=1;b=1;
+#10 $finish;
+end
+endmodule
