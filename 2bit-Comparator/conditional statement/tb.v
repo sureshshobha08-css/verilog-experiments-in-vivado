71dcb8203a00bdd6ffac59de40b07ab7e260448e
Shobha J S <sureshshobha54@gmail.com>
2026-08-29 22:42:52 +0530

    Update tb.v

--- 2bit-Comparator/conditional statement/tb.v
+++ 2bit-Comparator/conditional statement/tb.v
@@ -1,1 +1,25 @@
-
+module top;
+reg[1:0]a,b;
+wire equal,greater,smaller;
+comparator_2bit u1(a,b,equal,greater,smaller);
+initial
+begin
+a=2'b00;b=2'b00;
+#10;a=2'b00;b=2'b01;
+#10;a=2'b00;b=2'b10;
+#10;a=2'b00;b=2'b11;
+#10;a=2'b01;b=2'b00;
+#10;a=2'b01;b=2'b01;
+#10;a=2'b01;b=2'b10;
+#10;a=2'b01;b=2'b11;
+#10;a=2'b10;b=2'b00;
+#10;a=2'b10;b=2'b01;
+#10;a=2'b10;b=2'b10;
+#10;a=2'b10;b=2'b11;
+#10;a=2'b11;b=2'b00;
+#10;a=2'b11;b=2'b01;
+#10;a=2'b11;b=2'b10;
+#10;a=2'b11;b=2'b11;
+#10 $finish;
+end
+endmodule
