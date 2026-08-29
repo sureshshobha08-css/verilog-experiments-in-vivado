5aa1bc0e1714281edd9d81d4d0993ec6bef8c059
Shobha J S <sureshshobha54@gmail.com>
2026-08-29 18:40:10 +0530

    Create source.v

---
+++ full adder using 3X8 decoder/using OR gate at output side/source.v
@@ -0,0 +1,13 @@
+module FA_decoder_3x8(input a,b,cin,output sum,cout);
+wire[7:0]y;
+assign y[0]=~a & ~b & ~cin;
+assign y[1]=~a & ~b & cin;
+assign y[2]=~a & b & ~cin;
+assign y[3]=~a & b & cin;
+assign y[4]=a & ~b & ~cin;
+assign y[5]=a & ~b & cin;
+assign y[6]=a & b & ~cin;
+assign y[7]=a & b & cin;
+assign sum=y[1] | y[2] | y[4] | y[7];
+assign cout=y[3] | y[5] | y[6] | y[7];
+endmodule
