4c51e6a566d62e695e0f3c8a611472311915ff29
Shobha J S <sureshshobha54@gmail.com>
2026-08-29 19:30:47 +0530

    Update source.v

--- Octal to binary encoder/source.v
+++ Octal to binary encoder/source.v
@@ -1,1 +1,6 @@
+module octal_to_binary_encoder(input[7:0]octal_in,output[2:0]binary_out);
+assign binary_out[0]=octal_in[1]|octal_in[3]|octal_in[5]|octal_in[7];
+assign binary_out[1]=octal_in[2]|octal_in[3]|octal_in[6]|octal_in[7];
+assign binary_out[2]=octal_in[4]|octal_in[5]|octal_in[6]|octal_in[7];
 
+endmodule
