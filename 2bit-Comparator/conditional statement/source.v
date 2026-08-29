f96e1c82423ad8f63d2da8666f09aee341c81a52
Shobha J S <sureshshobha54@gmail.com>
2026-08-29 22:42:24 +0530

    Update source.v

--- 2bit-Comparator/conditional statement/source.v
+++ 2bit-Comparator/conditional statement/source.v
@@ -1,1 +1,12 @@
-
+module comparator_2bit(input[1:0] a,b,output reg equal,greater,smaller);
+always@(*)
+begin
+equal=0;greater=0;smaller=0;
+if(a==b)
+equal=1;
+else if(a>b)
+greater=1;
+else 
+smaller=1;
+end
+endmodule
